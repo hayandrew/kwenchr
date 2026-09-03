33a6835be3d7162da54664384bc738402bf14ee4
Andy Hay <andy@andyhay.com>
2026-09-03 09:56:12 -0400

    adding async to google maps api

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,8 +31,9 @@ export default function RootLayout({ children }) {
         
         {/* Google Maps API with Places Library for Location Search */}
         <Script 
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}&libraries=places`}
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}&libraries=places&loading=async`}
           strategy="afterInteractive"
+          async
         />
       </body>
     </html>
